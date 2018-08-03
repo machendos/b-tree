cb1e4dc2e33d67565c7939b2c9f82b49da5181f4
Nicita01 <nicitamac@gmail.com>
2018-08-03 18:08:54 +0300

    Define BTree intrface

---
+++ b-tree.js
@@ -0,0 +1,44 @@
+'use strict';
+
+function BTree() {
+
+}
+
+function BTreeNode() {
+  this.value = undefined;
+  this.typle = null;
+  this.child = null;
+}
+
+methods.add = function(
+  value, // number or string
+  typle // related data
+) {
+
+};
+
+methods.delete = function(value) {
+
+};
+
+methods.getEqual = function(value) {
+  
+};
+
+methods.getLarger = function(value) {
+
+};
+
+methods.getLess = function(value) {
+  
+};
+
+methods.getBetween = function(
+  startValue,
+  finishValue
+) {
+
+}
+const methods = {};
+
+BTree.prototype = Object.assign({}, methods);
